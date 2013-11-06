10c1c7990580d5ffee74c5c094543733ced52b8a
Tom Adams <tom@dxw.com>
2013-11-06 09:34:57 -0500

    Add blockToIpAndNetmask function

--- src/CIDR/IPv4.php
+++ src/CIDR/IPv4.php
@@ -30,4 +30,28 @@ class IPv4 {
 
     return [$int, null];
   }
+
+  static function block_to_ip_and_netmask($block) {
+    $split = explode('/', $block);
+
+    if (count($split) !== 2) {
+      return ['', 0, true];
+    }
+
+    $ip = $split[0];
+    $netmask = (int)$split[1];
+
+    if ($netmask < 0 || $netmask > 32) {
+      return ['', 0, true];
+    }
+
+    // 0 => 00000000000000000000000000000000
+    // 1 => 10000000000000000000000000000000
+    // 2 => 11000000000000000000000000000000
+    // 3 => 11100000000000000000000000000000
+    // etc
+    $_netmask = pow(2, $netmask)-1 << (32-$netmask);
+
+    return [$ip, $_netmask, null];
+  }
 }
--- test/ipv4_test.php
+++ test/ipv4_test.php
@@ -1,10 +1,7 @@
 <?php
 
 class IPv4Test extends PHPUnit_Framework_TestCase {
-  function testToInt() {
-
-    // Errors
-
+  function testToIntGeneratesErrors() {
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.00');
     $this->assertEquals($err, true);
     $this->assertEquals($int, 0);
@@ -12,23 +9,40 @@ class IPv4Test extends PHPUnit_Framework_TestCase {
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.0.256');
     $this->assertEquals($err, true);
     $this->assertEquals($int, 0);
+  }
 
-    // No errors
-
+  function testToIntReturnsCorrectIntegers() {
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.0.0');
     $this->assertEquals($err, null);
-    $this->assertEquals($int, 0);
+    $this->assertEquals($int, 0b00000000000000000000000000000000);
 
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.0.255');
     $this->assertEquals($err, null);
-    $this->assertEquals($int, 255);
+    $this->assertEquals($int, 0b00000000000000000000000011111111);
 
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.1.0');
     $this->assertEquals($err, null);
-    $this->assertEquals($int, 256);
+    $this->assertEquals($int, 0b00000000000000000000000100000000);
 
     list($int, $err) = \CIDR\IPv4::addr_to_int('0.0.1.1');
     $this->assertEquals($err, null);
-    $this->assertEquals($int, 257);
+    $this->assertEquals($int, 0b00000000000000000000000100000001);
+  }
+
+  function testBlockToIpAndNetmask() {
+    list($ip, $netmask, $err) = \CIDR\IPv4::block_to_ip_and_netmask('192.168.1.1/24');
+    $this->assertEquals($err, null);
+    $this->assertEquals($ip, '192.168.1.1');
+    $this->assertEquals($netmask, \CIDR\IPv4::addr_to_int('255.255.255.0')[0]);
+
+    list($ip, $netmask, $err) = \CIDR\IPv4::block_to_ip_and_netmask('192.168.1.1/0');
+    $this->assertEquals($err, null);
+    $this->assertEquals($ip, '192.168.1.1');
+    $this->assertEquals($netmask, \CIDR\IPv4::addr_to_int('0.0.0.0')[0]);
+
+    list($ip, $netmask, $err) = \CIDR\IPv4::block_to_ip_and_netmask('192.168.1.1/1');
+    $this->assertEquals($err, null);
+    $this->assertEquals($ip, '192.168.1.1');
+    $this->assertEquals($netmask, \CIDR\IPv4::addr_to_int('128.0.0.0')[0]);
   }
 }
