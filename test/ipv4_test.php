d98044c29d52ec69b28309034be5a60e21871cb6
Tom Adams <tom@dxw.com>
2015-06-24 16:00:38 -0400

    DRY the tests

--- test/ipv4_test.php
+++ test/ipv4_test.php
@@ -2,96 +2,89 @@
 
 class IPv4Test extends PHPUnit_Framework_TestCase {
   function testToIntGeneratesErrors() {
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.00');
-    $this->assertEquals($err, true);
+    $matrix = [
+      '0.0.00',
+      '0.0.0.256',
+      ["passing an array"],
+    ];
 
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.0.256');
-    $this->assertEquals($err, true);
-
-    list($int, $err) = \CIDR\IPv4::addrToInt(["passing an array"]);
-    $this->assertEquals($err, true);
+    foreach ($matrix as $addr) {
+      list($int, $err) = \CIDR\IPv4::addrToInt($addr);
+      $this->assertEquals($err, true);
+    }
   }
 
   function testToIntReturnsCorrectIntegers() {
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.0.0');
-    $this->assertEquals($err, null);
-    $this->assertEquals($int, 0b00000000000000000000000000000000);
+    $matrix = [
+      ['0.0.0.0',   0b00000000000000000000000000000000],
+      ['0.0.0.255', 0b00000000000000000000000011111111],
+      ['0.0.1.0',   0b00000000000000000000000100000000],
+      ['0.0.1.1',   0b00000000000000000000000100000001],
+    ];
 
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.0.255');
-    $this->assertEquals($err, null);
-    $this->assertEquals($int, 0b00000000000000000000000011111111);
-
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.1.0');
-    $this->assertEquals($err, null);
-    $this->assertEquals($int, 0b00000000000000000000000100000000);
-
-    list($int, $err) = \CIDR\IPv4::addrToInt('0.0.1.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($int, 0b00000000000000000000000100000001);
+    foreach ($matrix as $row) {
+      list($int, $err) = \CIDR\IPv4::addrToInt($row[0]);
+      $this->assertEquals($err, null);
+      $this->assertEquals($int, $row[1]);
+    }
   }
 
   function testNetmaskGeneratesErrors() {
-    list($netmask, $err) = \CIDR\IPv4::netmask(129);
-    $this->assertEquals($err, true);
+    $matrix = [
+      129,
+      '123',
+      'abc',
+    ];
 
-    list($netmask, $err) = \CIDR\IPv4::netmask('123');
-    $this->assertEquals($err, true);
-
-    list($netmask, $err) = \CIDR\IPv4::netmask('abc');
-    $this->assertEquals($err, true);
+    foreach ($matrix as $netmask) {
+      list($netmask, $err) = \CIDR\IPv4::netmask($netmask);
+      $this->assertEquals($err, true);
+    }
   }
 
   function testNetmask() {
-    list($netmask, $err) = \CIDR\IPv4::netmask(24);
-    $this->assertEquals($err, null);
-    $this->assertEquals($netmask, \CIDR\IPv4::addrToInt('255.255.255.0')[0]);
+    $matrix = [
+      [24, '255.255.255.0'],
+      [0, '0.0.0.0'],
+      [1, '128.0.0.0'],
+    ];
 
-    list($netmask, $err) = \CIDR\IPv4::netmask(0);
-    $this->assertEquals($err, null);
-    $this->assertEquals($netmask, \CIDR\IPv4::addrToInt('0.0.0.0')[0]);
 
-    list($netmask, $err) = \CIDR\IPv4::netmask(1);
-    $this->assertEquals($err, null);
-    $this->assertEquals($netmask, \CIDR\IPv4::addrToInt('128.0.0.0')[0]);
+    foreach ($matrix as $row) {
+      list($netmask, $err) = \CIDR\IPv4::netmask($row[0]);
+      $this->assertEquals($err, null);
+      $this->assertEquals($netmask, \CIDR\IPv4::addrToInt($row[1])[0]);
+    }
   }
 
   function testMatchGeneratesErrors() {
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/224', '192.168.1.1');
-    $this->assertEquals($err, true);
+    $matrix = [
+      ['192.168.1.1/224',  '192.168.1.1'],
+      ['192.168.1.991/24', '192.168.1.1'],
+      ['192.168.1.1/24',   '192.168.1.991'],
+      ['192.168.1.1/abc',  '192.168.1.1'],
+    ];
 
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.991/24', '192.168.1.1');
-    $this->assertEquals($err, true);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/24', '192.168.1.991');
-    $this->assertEquals($err, true);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/abc', '192.168.1.1');
-    $this->assertEquals($err, true);
+    foreach ($matrix as $row) {
+      list($match, $err) = \CIDR\IPv4::match($row[0], $row[1]);
+      $this->assertEquals($err, true);
+    }
   }
 
   function testMatch() {
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/24', '192.168.1.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, true);
+    $matrix = [
+      ['192.168.1.1/24', '192.168.1.1',   true],
+      ['192.168.1.1/24', '192.168.1.255', true],
+      ['192.168.1.1/0',  '127.0.0.1',     true],
+      ['192.168.1.1/24', '192.168.2.1',   false],
+      ['192.168.1.1',    '192.168.2.1',   false],
+      ['192.168.1.1',    '192.168.1.1',   true],
+    ];
 
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/24', '192.168.1.255');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, true);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/0', '127.0.0.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, true);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1/24', '192.168.2.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, false);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1', '192.168.2.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, false);
-
-    list($match, $err) = \CIDR\IPv4::match('192.168.1.1', '192.168.1.1');
-    $this->assertEquals($err, null);
-    $this->assertEquals($match, true);
+    foreach ($matrix as $row) {
+      list($match, $err) = \CIDR\IPv4::match($row[0], $row[1]);
+      $this->assertEquals($err, null);
+      $this->assertEquals($match, $row[2]);
+    }
   }
 }
