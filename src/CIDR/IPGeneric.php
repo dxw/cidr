b6abfd9a202013c5142af14c1891edd057e7818a
Tom Adams <tom@dxw.com>
2015-06-30 16:02:50 -0400

    Make the IPv6 tests pass

--- src/CIDR/IPGeneric.php
+++ src/CIDR/IPGeneric.php
@@ -14,7 +14,8 @@ abstract class IPGeneric {
       return [0, true];
     }
 
-    $netmask = pow(2, $i)-1 << ($this->bits-$i);
+    // $netmask = pow(2, $i)-1 << ($this->bits-$i);
+    $netmask = bcmul(bcsub(bcpow(2, $i), 1), bcpow(2, $this->bits-$i));
 
     return [$netmask, null];
   }
@@ -37,7 +38,7 @@ abstract class IPGeneric {
 
     if ($haystack_netmask === null) {
 
-      return [$_haystack_addr === $_needle, null];
+      return [bccomp($_haystack_addr, $_needle) === 0, null];
 
     } else {
 
@@ -52,12 +53,73 @@ abstract class IPGeneric {
         return [null, $err];
       }
 
-      $haystack_masked = $_haystack_addr & $_haystack_netmask;
-      $needle_masked = $_needle & $_haystack_netmask;
-
-      $match = $haystack_masked === $needle_masked;
+      $haystack_masked = static::bcAnd($_haystack_addr, $_haystack_netmask);
+      $needle_masked = static::bcAnd($_needle, $_haystack_netmask);
+      $match = bccomp($haystack_masked, $needle_masked) === 0;
 
       return [$match, null];
     }
+  }
+
+  // & that works with BC numbers
+  static function bcAnd($a, $b) {
+    // If a is longer than b, swap
+    if (strlen($a) > strlen($b)) {
+      $c = $b;
+      $b = $a;
+      $a = $c;
+    }
+
+    // Adjust a until it's as long as b
+    while (strlen($b) > strlen($a)) {
+      $a = '0' . $a;
+    }
+
+    // Convert to hex
+    $_a = static::dec2hex($a);
+    $_b = static::dec2hex($b);
+
+    // & one block at a time
+    $c = '';
+    for ($i = 0; $i < strlen($_a); $i += 8) {
+      $aBlock = substr($_a, $i, 8);
+      $bBlock = substr($_b, $i, 8);
+
+      $c .= hexdec($aBlock) & hexdec($bBlock);
+    }
+
+    // Convert to dec
+    return static::hex2dec($c);
+  }
+
+  // http://php.net/manual/en/function.dechex.php#21086
+  static function hex2dec($number)
+  {
+    $decvalues = array('0' => '0', '1' => '1', '2' => '2',
+    '3' => '3', '4' => '4', '5' => '5',
+    '6' => '6', '7' => '7', '8' => '8',
+    '9' => '9', 'A' => '10', 'B' => '11',
+    'C' => '12', 'D' => '13', 'E' => '14',
+    'F' => '15');
+    $decval = '0';
+    $number = strrev($number);
+    for($i = 0; $i < strlen($number); $i++)
+    {
+      $decval = bcadd(bcmul(bcpow('16',$i,0),$decvalues[$number{$i}]), $decval);
+    }
+    return $decval;
+  }
+
+  // http://php.net/manual/en/function.dechex.php#21086
+  static function dec2hex($number) {
+    $hexvalues = array('0','1','2','3','4','5','6','7',
+    '8','9','A','B','C','D','E','F');
+    $hexval = '';
+    while($number != '0')
+    {
+      $hexval = $hexvalues[bcmod($number,'16')].$hexval;
+      $number = bcdiv($number,'16',0);
+    }
+    return $hexval;
   }
 }
--- src/CIDR/IPv6.php
+++ src/CIDR/IPv6.php
@@ -12,10 +12,69 @@ class IPv6 extends IPGeneric {
       return [0, true];
     }
 
-    $int = ip2long($addr);
+    $addr = strtolower($addr);
 
-    if ($int === false) {
-      return [0, true];
+    // Parse the string
+
+    $ast = [];
+    $pos = 0;
+
+    for ($i = 0; $i < strlen($addr); $i++) {
+      $c = substr($addr, $i, 1);
+
+      $prev = $pos < 1 ? 0 : $pos - 1;
+
+      if ($c === ':') {
+        if (isset($ast[$prev]) && $ast[$prev] === ':') {
+          $ast[$prev] = '::';
+        } else {
+          $ast[$pos] = ':';
+          $pos++;
+        }
+      } elseif (in_array($c, ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'])) {
+        if (!isset($ast[$prev]) || $ast[$prev] === ':' || $ast[$prev] === '::') {
+          $ast[$pos] = $c;
+          $pos++;
+        } else {
+          $ast[$prev] .= $c;
+        }
+      } else {
+        return [0, true];
+      }
+    }
+
+    // Strip the separators
+
+    $ast = array_filter($ast, function ($a) { return $a !== ':'; });
+
+    // Convert each block to an integer
+
+    $blocks = [];
+    $i = 0;
+
+    foreach ($ast as $cur) {
+      if ($cur === '::') {
+        // fill in the blanks
+        for ($j = 0; $j < 9 - count($ast); $j++) {
+          $blocks[] = 0;
+        }
+      } else {
+        $val = hexdec($cur);
+        if ($val > 0xffff) {
+          return [0, true];
+        }
+        $blocks[] = $val;
+      }
+
+      $i++;
+    }
+
+    // Add together
+
+    $int = 0;
+
+    for ($i = 0; $i < count($blocks); $i++) {
+      $int = bcadd($int, bcmul($blocks[$i], bcpow(2, (7-$i) * 16)));
     }
 
     return [$int, null];
