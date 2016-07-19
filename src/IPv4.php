459862870da0559b10a5768f12a1e5950689e55d
Tom Adams <tom@dxw.com>
2016-07-19 10:01:41 -0400

    Use \Dxw\Result\Result instead of \Result\Result
    - Make use of the ->wrap() method to simplify code
    - Update .travis.yml to only test PHP 7.0

--- src/IPv4.php
+++ src/IPv4.php
@@ -7,31 +7,31 @@ class IPv4
     public static function addrToInt($addr)
     {
         if (!is_string($addr)) {
-            return \Result\Result::err('not a string');
+            return \Dxw\Result\Result::err('not a string');
         }
 
         $int = ip2long($addr);
 
         if ($int === false) {
-            return \Result\Result::err('ip2long returned error');
+            return \Dxw\Result\Result::err('ip2long returned error');
         }
 
-        return \Result\Result::ok($int);
+        return \Dxw\Result\Result::ok($int);
     }
 
     public static function netmask($i)
     {
         if (!is_int($i)) {
-            return \Result\Result::err('$i not of type int');
+            return \Dxw\Result\Result::err('$i not of type int');
         }
 
         if ($i < 0 || $i > 32) {
-            return \Result\Result::err('$i out of range');
+            return \Dxw\Result\Result::err('$i out of range');
         }
 
         $netmask = pow(2, $i) - 1 << (32 - $i);
 
-        return \Result\Result::ok($netmask);
+        return \Dxw\Result\Result::ok($netmask);
     }
 
     public static function match($haystack, $needle)
@@ -43,29 +43,29 @@ class IPv4
 
         $result = self::addrToInt($haystack_addr);
         if ($result->isErr()) {
-            return \Result\Result::err(sprintf('address portion of $haystack invalid: %s', $result->getErr()));
+            return $result->wrap('address portion of $haystack invalid');
         }
         $_haystack_addr = $result->unwrap();
 
         $result = self::addrToInt($needle);
         if ($result->isErr()) {
-            return \Result\Result::err(sprintf('$needle invalid: %s', $result->getErr()));
+            return $result->wrap('$needle invalid');
         }
         $_needle = $result->unwrap();
 
         if ($haystack_netmask === null) {
-            return \Result\Result::ok($_haystack_addr === $_needle);
+            return \Dxw\Result\Result::ok($_haystack_addr === $_needle);
         } else {
 
             // Make sure string is valid int
             $haystack_netmask_i = (int) $haystack_netmask;
             if ($haystack_netmask !== (string) $haystack_netmask_i) {
-                return \Result\Result::err('$haystack contains invalid netmask');
+                return \Dxw\Result\Result::err('$haystack contains invalid netmask');
             }
 
             $result = self::netmask($haystack_netmask_i);
             if ($result->isErr()) {
-                return \Result\Result::err(sprintf('netmask portion of $haystack invalid: %s', $result->getErr()));
+                return $result->wrap('netmask portion of $haystack invalid');
             }
             $_haystack_netmask = $result->unwrap();
 
@@ -74,7 +74,7 @@ class IPv4
 
             $match = $haystack_masked === $needle_masked;
 
-            return \Result\Result::ok($match);
+            return \Dxw\Result\Result::ok($match);
         }
     }
 }
