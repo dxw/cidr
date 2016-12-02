519b84ac7b46d470469936f578f906c78bfef781
Tom Adams <tom@dxw.com>
2016-12-02 14:17:15 -0500

    Add integration tests

--- spec/ip.spec.php
+++ spec/ip.spec.php
@@ -1,98 +1,155 @@
 <?php
 
 describe(\Dxw\CIDR\IP::class, function () {
-    beforeEach(function () {
-        $this->mock = \Mockery::mock('nothing');
-        \Dxw\CIDR\IP::$IPAddress = [$this->mock, 'IPAddress'];
-        \Dxw\CIDR\IP::$IPRange = [$this->mock, 'IPRange'];
-    });
-
-    describe('::contains()', function () {
+    context('unit tests (with injection)', function () {
         beforeEach(function () {
-            $this->address = 'x';
-            $this->range = 'y';
-
-            $this->expectError = function (string $err) {
-                $result = \Dxw\CIDR\IP::contains($this->range, $this->address);
-                expect($result)->to->be->instanceof(\Dxw\Result\Result::class);
-                expect($result->isErr())->to->equal(true);
-                expect($result->getErr())->to->equal($err);
-            };
-
-            $this->expectOkay = function (bool $output) {
-                $result = \Dxw\CIDR\IP::contains($this->range, $this->address);
-                expect($result)->to->be->instanceof(\Dxw\Result\Result::class);
-                expect($result->isErr())->to->equal(false);
-                expect($result->unwrap())->to->equal($output);
-            };
+            $this->mock = \Mockery::mock('nothing');
+            \Dxw\CIDR\IP::$IPAddress = [$this->mock, 'IPAddress'];
+            \Dxw\CIDR\IP::$IPRange = [$this->mock, 'IPRange'];
         });
 
-        // context address is nonsense
-        // context address is valid
-        //   context range is nonsense
-        //   context range is valid
-        //     context range contains address
-        //     context range does not contain address
-
-        context('address is nonsense', function () {
-            beforeEach(function () {
-                $this->mock->shouldReceive('IPAddress')->withArgs([$this->address])->andReturn(
-                    \Dxw\Result\Result::err('invalid address')
-                );
-            });
-
-            it('returns error', function () {
-                $this->expectError('invalid address');
-            });
+        afterEach(function () {
+            \Dxw\CIDR\IP::$IPAddress = [\Dxw\CIDR\IPAddress::class, 'Make'];
+            \Dxw\CIDR\IP::$IPRange = [\Dxw\CIDR\IPRange::class, 'Make'];
         });
 
-        context('address is valid', function () {
+        describe('::contains()', function () {
             beforeEach(function () {
-                $this->ipAddress = \Mockery::mock(\Dxw\CIDR\IPv6Address::class);
-                $this->mock->shouldReceive('IPAddress')->withArgs([$this->address])->andReturn(
-                    \Dxw\Result\Result::ok($this->ipAddress)
-                );
+                $this->address = 'x';
+                $this->range = 'y';
+
+                $this->expectError = function (string $err) {
+                    $result = \Dxw\CIDR\IP::contains($this->range, $this->address);
+                    expect($result)->to->be->instanceof(\Dxw\Result\Result::class);
+                    expect($result->isErr())->to->equal(true);
+                    expect($result->getErr())->to->equal($err);
+                };
+
+                $this->expectOkay = function (bool $output) {
+                    $result = \Dxw\CIDR\IP::contains($this->range, $this->address);
+                    expect($result)->to->be->instanceof(\Dxw\Result\Result::class);
+                    expect($result->isErr())->to->equal(false);
+                    expect($result->unwrap())->to->equal($output);
+                };
             });
 
-            context('range is nonsense', function () {
+            // context address is nonsense
+            // context address is valid
+            //   context range is nonsense
+            //   context range is valid
+            //     context range contains address
+            //     context range does not contain address
+
+            context('address is nonsense', function () {
                 beforeEach(function () {
-                    $this->mock->shouldReceive('IPRange')->withArgs([$this->range])->andReturn(
-                        \Dxw\Result\Result::err('invalid range')
+                    $this->mock->shouldReceive('IPAddress')->withArgs([$this->address])->andReturn(
+                        \Dxw\Result\Result::err('invalid address')
                     );
                 });
 
                 it('returns error', function () {
-                    $this->expectError('invalid range');
+                    $this->expectError('invalid address');
                 });
             });
 
-            context('range is valid', function () {
+            context('address is valid', function () {
                 beforeEach(function () {
-                    $this->ipRange = \Mockery::mock(\Dxw\CIDR\IPv6Range::class);
-                    $this->mock->shouldReceive('IPRange')->withArgs([$this->range])->andReturn(
-                        \Dxw\Result\Result::ok($this->ipRange)
+                    $this->ipAddress = \Mockery::mock(\Dxw\CIDR\IPv6Address::class);
+                    $this->mock->shouldReceive('IPAddress')->withArgs([$this->address])->andReturn(
+                        \Dxw\Result\Result::ok($this->ipAddress)
                     );
                 });
 
-                context('range does not contain address', function () {
+                context('range is nonsense', function () {
                     beforeEach(function () {
-                        $this->ipRange->shouldReceive('containsAddress')->withArgs([$this->ipAddress])->andReturn(false);
+                        $this->mock->shouldReceive('IPRange')->withArgs([$this->range])->andReturn(
+                            \Dxw\Result\Result::err('invalid range')
+                        );
                     });
 
-                    it('returns false', function () {
-                        $this->expectOkay(false);
+                    it('returns error', function () {
+                        $this->expectError('invalid range');
                     });
                 });
 
-                context('range contains address', function () {
+                context('range is valid', function () {
                     beforeEach(function () {
-                        $this->ipRange->shouldReceive('containsAddress')->withArgs([$this->ipAddress])->andReturn(true);
+                        $this->ipRange = \Mockery::mock(\Dxw\CIDR\IPv6Range::class);
+                        $this->mock->shouldReceive('IPRange')->withArgs([$this->range])->andReturn(
+                            \Dxw\Result\Result::ok($this->ipRange)
+                        );
                     });
 
-                    it('returns true', function () {
-                        $this->expectOkay(true);
+                    context('range does not contain address', function () {
+                        beforeEach(function () {
+                            $this->ipRange->shouldReceive('containsAddress')->withArgs([$this->ipAddress])->andReturn(false);
+                        });
+
+                        it('returns false', function () {
+                            $this->expectOkay(false);
+                        });
+                    });
+
+                    context('range contains address', function () {
+                        beforeEach(function () {
+                            $this->ipRange->shouldReceive('containsAddress')->withArgs([$this->ipAddress])->andReturn(true);
+                        });
+
+                        it('returns true', function () {
+                            $this->expectOkay(true);
+                        });
                     });
                 });
+            });
+        });
+    });
+
+    context('integration tests', function () {
+        describe('::contains()', function () {
+            context('nonsense range', function () {
+                it('returns error', function () {
+                    $result = \Dxw\CIDR\IP::contains('/', '::');
+                    expect($result->isErr())->to->be->true();
+                    expect($result->getErr())->to->equal('could not parse range as IPv6 or IPv4');
+                });
+            });
+
+            context('nonsense address', function () {
+                it('returns error', function () {
+                    $result = \Dxw\CIDR\IP::contains('::1', '/');
+                    expect($result->isErr())->to->be->true();
+                    expect($result->getErr())->to->equal('could not parse address as IPv6 or IPv4');
+                });
+            });
+
+            context('mismatched address types', function () {
+                it('returns false', function () {
+                    $result = \Dxw\CIDR\IP::contains('::/64', '127.0.0.1');
+                    expect($result->isErr())->to->be->false();
+                    expect($result->unwrap())->to->be->false();
+                });
+            });
+
+            context('matching IPv6', function () {
+                it('returns true', function () {
+                    $result = \Dxw\CIDR\IP::contains('::/64', '::1');
+                    expect($result->isErr())->to->be->false();
+                    expect($result->unwrap())->to->be->true();
+                });
+            });
+
+            context('matching IPv4', function () {
+                it('returns true', function () {
+                    $result = \Dxw\CIDR\IP::contains('127.0.0.0/24', '127.0.0.1');
+                    expect($result->isErr())->to->be->false();
+                    expect($result->unwrap())->to->be->true();
+                });
+            });
+
+            context('matching IPv4-mapped IPv6 address', function () {
+                $result = \Dxw\CIDR\IP::contains('::ffff:7f00:0001', '::ffff:127.0.0.1');
+                expect($result->isErr())->to->be->false();
+                expect($result->unwrap())->to->be->true();
             });
         });
     });
